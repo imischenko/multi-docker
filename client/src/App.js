2330ef9b7feb5038ff6f24aafcf8b7e3ae32af9c
ATB | Ivan Mischenko <imischenko@atbdev.ru>
2021-10-23 13:00:36 +0300

    Updating from zip file

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
@@ -22,10 +23,10 @@ function App() {
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
-            <Route exact path="/" component={Fib} />
-            <Route exact path="/otherpage" component={OtherPage} />
-          </div>
-      </div>      
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
+      </div>
     </Router>
   );
 }
